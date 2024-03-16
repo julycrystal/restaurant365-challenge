93129a3d5d4f3e58ad118830b64f48c086ee627c
julycrystal <131466754+julycrystal@users.noreply.github.com>
2024-03-15 22:01:21 -0400

    feat: milestone 1 - support maximum of 2 separated by comma

--- restaurant365/Program.cs
+++ restaurant365/Program.cs
@@ -2,7 +2,19 @@
 {
   public int Add(string input)
   {
-    return 0;
+    List<int> numbers = SplitNumbers(input);
+    if (numbers.Count > 2) throw new Exception("Supports a maximum of 2 numbers");
+
+    return numbers.Sum();
+  }
+
+  private List<int> SplitNumbers(string input)
+  {
+    List<int> numbers = input.Split(",")
+                             .Select(n => int.TryParse(n, out int parsed) ? parsed : 0)
+                             .ToList();
+
+    return numbers;
   }
 }
 
