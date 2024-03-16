d5756097d6fbbe3ba0a7ba2ea9bede44993acd1f
julycrystal <131466754+julycrystal@users.noreply.github.com>
2024-03-15 21:59:09 -0400

    feat: scaffolding - input,add,output loop

--- restaurant365/Program.cs
+++ restaurant365/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Calculator
+{
+  public int Add(string input)
+  {
+    return 0;
+  }
+}
+
+namespace StringCalculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Calculator calculator = new Calculator();
+
+      while (true)
+      {
+        string? input = Console.ReadLine();
+        if (input == null) return;
+
+        try
+        {
+          int result = calculator.Add(input);
+          Console.WriteLine("Result: " + result);
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine("Error: " + ex.Message);
+        }
+      }
+    }
+  }
+}
